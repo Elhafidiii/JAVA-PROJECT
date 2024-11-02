ff4c6c0e8307bb042ec00d92b8a31537f043142a
Elhafidiii <m.amineelhafidi@gmail.com>
2024-11-02 19:50:00 +0100

    init commit

---
+++ src/main/java/com/example/manic_time/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.manic_time;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.manic_time {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.manic_time to javafx.fxml;
+    exports com.example.manic_time;
+}
